aa471791375f16d0fbf0793e38db663fdb0808e3
ven <v@ven.earth>
2021-06-30 18:49:33 +0100

    chore: update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class AppDocument extends Document {
   render() {
@@ -11,39 +11,25 @@ class AppDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
-          <link
-            rel="apple-touch-icon"
-            sizes="152x152"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
+          <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#FFFFFF" />
           <meta property="og:url" content="https://ven.earth" />
           <meta property="og:site_name" content="ven.earth" />
-          <meta property="og:image" content="/ven.jpg" />
+          <meta property="og:image" content="/img/ven.jpg" />
           <meta name="theme-color" content="#6a0dad" />
           <meta name="twitter:card" content="summary" />
-          <meta name="twitter:image" content="https://ven.earth/ven.jpg" />
+          <meta name="twitter:image" content="https://ven.earth/img/ven.jpg" />
         </Head>
         <body className="bg-white dark:bg-black text-black dark:text-white">
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default AppDocument;
+export default AppDocument
