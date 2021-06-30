6feb40a48dfed6dfbede4a2f918a7c1db1f56cfe
ven <v@ven.earth>
2021-06-30 18:31:59 +0100

    refactor: clean up api routes

--- src/pages/api/weather.ts
+++ src/pages/api/weather.ts
@@ -1,8 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+const { WEATHER_API_KEY } = process.env
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=${process.env.WEATHER_API_KEY}`
+    `http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=${WEATHER_API_KEY}`
   )
   const data = await response.json()
   res.status(200).json(data)
