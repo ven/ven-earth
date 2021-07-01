0167b6bf7876b22a0bc0f9db18378bba4c55cd2e
ven <v@ven.earth>
2021-07-01 03:01:09 +0100

    style: initial before animate

--- src/components/Nav/Dropdown.tsx
+++ src/components/Nav/Dropdown.tsx
@@ -43,8 +43,8 @@ export const Dropdown = () => {
       <AnimatePresence>
         {active && (
           <motion.div
-            animate={{ opacity: 1, scale: 1 }}
             initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.8 }}
             transition={{ duration: 0.3 }}
             onMouseLeave={() => setActive(false)}
