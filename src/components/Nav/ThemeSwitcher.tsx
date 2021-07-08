cffaa33275fa6013258b8bf45b5ea74774748b19
ven <v@ven.earth>
2021-07-08 17:51:46 +0100

    feat: rounded-lg on theme switcher

--- src/components/Nav/ThemeSwitcher.tsx
+++ src/components/Nav/ThemeSwitcher.tsx
@@ -28,7 +28,7 @@ export const ThemeSwitcher = () => {
     <button
       aria-label="Toggle Dark Mode"
       type="button"
-      className="focus:outline-none bg-none xs:bg-gray-200 xs:dark:bg-gray-800 rounded p-3 h-10 w-10 flex items-center justify-center"
+      className="focus:outline-none bg-none xs:bg-gray-200 xs:dark:bg-gray-800 rounded-lg p-3 h-10 w-10 flex items-center justify-center"
       onClick={() => toggle()}
     >
       <FontAwesomeIcon
