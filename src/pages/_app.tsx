d385add614cf6a08b923f2259441f86c524f5667
ven <v@ven.earth>
2021-07-09 01:55:55 +0100

    refactor: remove redundant code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,17 @@
-import "../styles/globals.css";
-import { Navbar } from "../components/Nav/Navbar";
-import { ThemeProvider } from "next-themes";
-import { Toaster } from "react-hot-toast";
-import { AppProps } from "next/app";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faTwitter,
-  faGithub,
-  faDiscord,
-  faSpotify,
-} from "@fortawesome/free-brands-svg-icons";
+import '../styles/globals.css'
+import { Navbar } from '../components/Nav/Navbar'
+import { ThemeProvider } from 'next-themes'
+import { Toaster } from 'react-hot-toast'
+import { AppProps } from 'next/app'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTwitter, faGithub, faDiscord, faSpotify } from '@fortawesome/free-brands-svg-icons'
 import {
   faEnvelope,
   faCalendarDay,
   faGlobeEurope,
   faCaretDown as faSolidCaretDown,
-} from "@fortawesome/free-solid-svg-icons";
-import { faClock } from "@fortawesome/free-regular-svg-icons";
+} from '@fortawesome/free-solid-svg-icons'
+import { faClock } from '@fortawesome/free-regular-svg-icons'
 import {
   faCloud,
   faClouds,
@@ -35,14 +30,14 @@ import {
   faCloudRain,
   faCloudSun,
   faClipboard,
-} from "@fortawesome/pro-regular-svg-icons";
-import { faSun } from "@fortawesome/pro-duotone-svg-icons";
+} from '@fortawesome/pro-regular-svg-icons'
+import { faSun } from '@fortawesome/pro-duotone-svg-icons'
 // The following import prevents a Font Awesome icon server-side rendering bug,
 // where the icons flash from a very large icon down to a properly sized one:
-import "@fortawesome/fontawesome-svg-core/styles.css";
+import '@fortawesome/fontawesome-svg-core/styles.css'
 // Prevent fontawesome from adding its CSS since we did it manually above:
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false; /* eslint-disable import/first */
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false /* eslint-disable import/first */
 
 library.add(
   faTwitter,
@@ -72,24 +67,20 @@ library.add(
   faCloudSun,
   faClipboard,
   faSun
-);
+)
 
 const App = ({ Component, pageProps, router }: AppProps) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
+      <Toaster position="bottom-left" />
       <div className="flex flex-col min-h-screen">
-        {router.pathname !== "/planet" && <Navbar />}
-        <Toaster position="bottom-left" />
-        <div
-          className={`h-full lg:px-0 ${
-            router.pathname !== "/planet" && "px-10"
-          }`}
-        >
+        <Navbar />
+        <div className="h-full lg:px-0 px-10">
           <Component {...pageProps} />
         </div>
       </div>
     </ThemeProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
