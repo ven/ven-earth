3a81e0ffaf0a482864a8f3b5f72f6968f2839f93
ven <v@ven.earth>
2021-07-10 20:17:02 +0100

    refactor: remove unnecessary classes from layout

--- src/layouts/BlogLayout.tsx
+++ src/layouts/BlogLayout.tsx
@@ -13,7 +13,7 @@ export const BlogLayout = ({ children, title, description }: Props) => {
     <>
       <GenericMeta title={title} description={description} />
 
-      <FadeIn className="w-full flex flex-col justify-center max-w-3xl mx-auto mb-16 sm:px-0">
+      <FadeIn className="flex flex-col justify-center max-w-3xl mx-auto mb-16 sm:px-0">
         <h1 className="text-5xl font-bold mr-4">{title}</h1>
         {children}
       </FadeIn>
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -14,7 +14,7 @@ export const MainLayout = ({ children, title, description, margin = true }: Prop
     <>
       {title && description && <GenericMeta title={title} description={description} />}
 
-      <FadeIn className="h-full w-full flex flex-col max-w-3xl mx-auto mb-16 sm:px-0">
+      <FadeIn className="flex flex-col max-w-3xl mx-auto mb-16 sm:px-0">
         {title && <h1 className="text-5xl font-bold">{title}</h1>}
         {description && <p className="text-gray-600 dark:text-gray-400 mt-2">{description}</p>}
         <div className={margin ? 'mt-8' : undefined}>{children}</div>
