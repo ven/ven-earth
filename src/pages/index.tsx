ba127c7b598ef84aa985d8fe5bad6bf1a3953b43
ven <46830208+ven@users.noreply.github.com>
2023-01-08 21:11:50 +0000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,13 +47,13 @@ const Home = () => {
     <>
       <GenericMeta
         title="ven.earth"
-        description="20-year-old software developer and student from the United Kingdom."
+        description="20-year-old software engineer and student from the United Kingdom."
       />
 
       <MainLayout margin={false}>
         <h1 className="text-6xl font-bold">ven</h1>
         <p className="text-lg text-gray-600 dark:text-gray-400 max-w-sm mt-2">
-          20-year-old <span className="font-semibold">software developer</span> and student from the
+          20-year-old <span className="font-semibold">software engineer</span> and student from the
           United Kingdom.
         </p>
         <div className="grid grid-flow-col w-48 mt-3 text-lg">
